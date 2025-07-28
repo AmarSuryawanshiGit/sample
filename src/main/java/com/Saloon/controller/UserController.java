cbe8844da743c6d47becbb71879529d28d194ada
Badalpatil <sathebadal8@gmail.com>
2025-07-29 00:21:17 +0530

    Feat: added main method

--- src/main/java/com/Saloon/controller/UserController.java
+++ src/main/java/com/Saloon/controller/UserController.java
@@ -1,8 +1,10 @@
 package com.Saloon.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RestController;
 
 @Controller
+@RestController
 public class UserController {
 
     public static void main(String[] args) {
